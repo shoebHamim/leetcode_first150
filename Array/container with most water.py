768853549feb6685f74b836fb2ce8282d94a7ce0
shoebHamim <hamim99bd@gmail.com>
2023-10-04 13:34:44 +0600

    max area.. no days off

---
+++ Array/container with most water.py
@@ -0,0 +1,15 @@
+def maxArea(height):
+  n=len(height)
+  l,r=0,n-1
+  max_area=0
+  while l<r:
+    area=(r-l)*min(height[l],height[r])
+    max_area=max(area,max_area)
+    if height[l]<height[r]:
+      l+=1
+    else:
+      r-=1
+  return max_area
+
+height =  height = [1,1]
+print(maxArea(height))
